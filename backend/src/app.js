27a8cc1390c4da54dd677f672b55cfa416a1eba2
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2023-06-13 20:05:56 -0300

    improvement(Fonte): Altera a fonte para Open Sans.

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const path = require('path');
 const errorMiddleware = require('./middlewares/error.middleware');
 
+const swagger = require('swagger-ui-express');
+const swaggerFile = require('./doc/swagger.json');
+
 const uploadPath = path.resolve('uploads');
 
 const routes = require('./routes');
@@ -14,6 +17,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/images', express.static(uploadPath));
+
+app.use('/docs', swagger.serve, swagger.setup(swaggerFile));
 
 // servindo arquivos estáticos da pasta dist
 const angularAppPath = path.resolve('../frontend/dist/frontend');
