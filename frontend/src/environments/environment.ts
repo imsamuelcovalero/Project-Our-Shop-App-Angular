6d551509126e4e2308264307cc42a39e657dfa82
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2023-07-05 19:20:35 -0300

    test(API_URL): Altera a API_URL de environment para não ter o problema de pegar o localhost, já que não está mudando para production

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 // environment.ts
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:3000'
+  // API_URL: 'http://localhost:3000'
+  API_URL: 'https://our-shop-app-backend-production.up.railway.app/'
 };
