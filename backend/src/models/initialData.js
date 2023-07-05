4ff51a270411c2af30bbb74796841f9a18b90f83
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2023-07-05 19:45:57 -0300

    fix(Backend API_URL): Cria uma variável de ambiente para passar o caminho das imagens de forma dinâmica no initialData.

--- backend/src/models/initialData.js
+++ backend/src/models/initialData.js
@@ -1,6 +1,11 @@
 /* initialData.js */
 const bcrypt = require('bcrypt');
 const { User, Product, WithdrawalPoint } = require('./models'); // Caminho para seus modelos
+require('dotenv').config();
+
+console.log('log de teste API_URL', process.env.API_URL);
+
+const URL_IMG = process.env.API_URL || 'http://localhost:3000';
 
 async function insertInitialData() {
   // Inserir usuário administrador
@@ -26,17 +31,17 @@ async function insertInitialData() {
 
   // Inserir produtos
   const products = [
-    { name: 'Skol Lata 350ml', price: 2.20, url_image: 'http://localhost:3000/images/skol_lata_350ml.jpg' },
-    { name: 'Heineken 600ml', price: 7.50, url_image: 'http://localhost:3000/images/heineken_600ml.jpg' },
-    { name: 'Antarctica Pilsen 300ml', price: 2.49, url_image: 'http://localhost:3000/images/antarctica_pilsen_300ml.jpg' },
-    { name: 'Brahma 600ml', price: 7.50, url_image: 'http://localhost:3000/images/brahma_600ml.jpg' },
-    { name: 'Skol 269ml', price: 2.19, url_image: 'http://localhost:3000/images/skol_269ml.jpg' },
-    { name: 'Skol Beats Senses 313ml', price: 4.49, url_image: 'http://localhost:3000/images/skol_beats_senses_313ml.jpg' },
-    { name: 'Becks 330ml', price: 4.99, url_image: 'http://localhost:3000/images/becks_330ml.jpg' },
-    { name: 'Brahma Duplo Malte 350ml', price: 2.79, url_image: 'http://localhost:3000/images/brahma_duplo_malte_350ml.jpg' },
-    { name: 'Becks 600ml', price: 8.89, url_image: 'http://localhost:3000/images/becks_600ml.jpg' },
-    { name: 'Skol Beats Senses 269ml', price: 3.57, url_image: 'http://localhost:3000/images/skol_beats_senses_269ml.jpg' },
-    { name: 'Stella Artois 275ml', price: 3.49, url_image: 'http://localhost:3000/images/stella_artois_275ml.jpg' }
+    { name: 'Skol Lata 350ml', price: 2.20, url_image: `${URL_IMG}/images/skol_lata_350ml.jpg` },
+    { name: 'Heineken 600ml', price: 7.50, url_image: `${URL_IMG}/images/heineken_600ml.jpg` },
+    { name: 'Antarctica Pilsen 300ml', price: 2.49, url_image: `${URL_IMG}/images/antarctica_pilsen_300ml.jpg` },
+    { name: 'Brahma 600ml', price: 7.50, url_image: `${URL_IMG}/images/brahma_600ml.jpg` },
+    { name: 'Skol 269ml', price: 2.19, url_image: `${URL_IMG}/images/skol_269ml.jpg` },
+    { name: 'Skol Beats Senses 313ml', price: 4.49, url_image: `${URL_IMG}/images/skol_beats_senses_313ml.jpg` },
+    { name: 'Becks 330ml', price: 4.99, url_image: `${URL_IMG}/images/becks_330ml.jpg` },
+    { name: 'Brahma Duplo Malte 350ml', price: 2.79, url_image: `${URL_IMG}/images/brahma_duplo_malte_350ml.jpg` },
+    { name: 'Becks 600ml', price: 8.89, url_image: `${URL_IMG}/images/becks_600ml.jpg` },
+    { name: 'Skol Beats Senses 269ml', price: 3.57, url_image: `${URL_IMG}/images/skol_beats_senses_269ml.jpg` },
+    { name: 'Stella Artois 275ml', price: 3.49, url_image: `${URL_IMG}/images/stella_artois_275ml.jpg` },
   ];
 
   // Verifica se os produtos já existem
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: false,
   // API_URL: 'http://localhost:3000'
-  API_URL: 'https://our-shop-app-backend-production.up.railway.app/'
+  API_URL: 'https://our-shop-app-backend-production.up.railway.app'
 };
